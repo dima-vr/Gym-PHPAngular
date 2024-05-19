0e9640b5f01d4e41a00b92f1b609b036ce82ff39
dimav <dima.vakarchuk1@gmail.com>
2024-05-20 02:25:34 +0300

    update UserConverter.java

--- back/sportapp/src/main/java/com/university/sportapp/dto/converter/UserConverter.java
+++ back/sportapp/src/main/java/com/university/sportapp/dto/converter/UserConverter.java
@@ -3,10 +3,22 @@ package com.university.sportapp.dto.converter;
 import com.university.sportapp.dto.request.UserRequest;
 import com.university.sportapp.dto.response.UserResponse;
 import com.university.sportapp.models.User;
+import com.university.sportapp.models.Role;
+import com.university.sportapp.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserConverter extends BaseConverter<UserRequest, UserResponse, User> {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+
+    public UserConverter(PasswordEncoder passwordEncoder, UserService userService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+
     @Override
     protected Class<User> getEntityClass() {
         return User.class;
@@ -15,5 +27,31 @@ public class UserConverter extends BaseConverter<UserRequest, UserResponse, User
     @Override
     protected Class<UserResponse> getResponseClass() {
         return UserResponse.class;
+    }
+
+    @Override
+    public User dtoToEntity(UserRequest request) {
+        User user = new User();
+        if (request.getId() != null) {
+            user = userService.get(request.getId());
+        }
+        if (request.getPassword() != null && !request.getPassword().isBlank()) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+        user.setEmail(request.getEmail());
+        user.setUsername(request.getUsername());
+        user.setHeight(request.getHeight());
+        user.setGoals(request.getGoals());
+        user.setWeight(request.getWeight());
+        user.setProfilePhotoUrl(request.getProfilePhotoUrl());
+        user.setPaymentStatus(request.isPaymentStatus());
+
+        if (request.getRole() == null) {
+            user.setRole(Role.USER);
+        } else {
+            user.setRole(request.getRole());
+        }
+
+        return user;
     }
 }
